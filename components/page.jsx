3a555ce9c02eca6fd953c5cc024824e318dca2ec
Wyatt Johnson <accounts+github@wyattjoh.ca>
2023-07-20 14:27:09 -0600

    feat: added more styles to diagnostic component

--- components/page.jsx
+++ components/page.jsx
@@ -18,6 +18,7 @@ function PageLink({ path, page, getStaticPaths = false }) {
         border: matchesPathname ? "1px solid blue" : "1px solid black",
         padding: "4px",
         marginBottom: "4px",
+        borderRadius: "4px",
       }}
     >
       <Link
@@ -33,7 +34,7 @@ function PageLink({ path, page, getStaticPaths = false }) {
         {router.basePath}
         {path}
       </Link>
-      <span style={{ fontFamily: "monospace" }}>
+      <span>
         {matchesPathname && <span> (pathname)</span>}
         {matchesPage && <span> (page)</span>}
         {getStaticPaths && <span> (getStaticPaths)</span>}
@@ -42,38 +43,78 @@ function PageLink({ path, page, getStaticPaths = false }) {
   );
 }
 
+function LegendItem({ name, children, style = {} }) {
+  return (
+    <div style={{ marginBottom: "15px", ...style }}>
+      <dt style={{ fontWeight: "bold", marginBottom: "5px" }}>({name})</dt>
+      <dd>{children}</dd>
+    </div>
+  );
+}
+
+function Header({ children }) {
+  return <h3 style={{ margin: "60px 0 30px" }}>{children}</h3>;
+}
+
 export function Page() {
   const router = useRouter();
 
   return (
-    <div style={{ margin: "30px auto", maxWidth: "700px" }}>
-      <h3>Route: {router.route}</h3>
-      <h2>Links</h2>
+    <div
+      style={{
+        margin: "30px auto",
+        maxWidth: "700px",
+        fontFamily: "monospace",
+      }}
+    >
       <div>
-        <PageLink
-          page="/[productSlug]/tutorials/[collectionSlug]"
-          path="/vault/tutorials/authentication"
-          getStaticPaths
-        />
-        <PageLink
-          page="/[productSlug]/tutorials/[collectionSlug]"
-          path="/vault/tutorials/auth"
-        />
-        <PageLink
-          page="/[productSlug]/tutorials/[...tutorialSlug]"
-          path="/vault/tutorials/authenticating/jwt"
-          getStaticPaths
-        />
-        <PageLink
-          page="/[productSlug]/tutorials/[...tutorialSlug]"
-          path="/vault/tutorials/authenticating/password"
-        />
-        <PageLink
-          page="/[productSlug]/tutorials"
-          path="/vault/tutorials"
-          getStaticPaths
-        />
-        <PageLink page="/" path="/" />
+        <Header>Route</Header>
+        <div>{router.route}</div>
+      </div>
+      <div>
+        <Header>Pathname</Header>
+        <div>{router.pathname}</div>
+      </div>
+      <div>
+        <Header>Links</Header>
+        <div>
+          <PageLink
+            page="/[productSlug]/tutorials/[collectionSlug]"
+            path="/vault/tutorials/authentication"
+            getStaticPaths
+          />
+          <PageLink
+            page="/[productSlug]/tutorials/[collectionSlug]"
+            path="/vault/tutorials/auth"
+          />
+          <PageLink
+            page="/[productSlug]/tutorials/[...tutorialSlug]"
+            path="/vault/tutorials/authenticating/jwt"
+            getStaticPaths
+          />
+          <PageLink
+            page="/[productSlug]/tutorials/[...tutorialSlug]"
+            path="/vault/tutorials/authenticating/password"
+          />
+          <PageLink
+            page="/[productSlug]/tutorials"
+            path="/vault/tutorials"
+            getStaticPaths
+          />
+          <PageLink page="/" path="/" />
+        </div>
+      </div>
+      <div>
+        <Header>Legend</Header>
+        <dl>
+          <LegendItem name="pathname" style={{ color: "blue" }}>
+            current page matches the pathname
+          </LegendItem>
+          <LegendItem name="page">current page matches the page</LegendItem>
+          <LegendItem name="getStaticPaths">
+            page was a listed in getStaticPaths
+          </LegendItem>
+        </dl>
       </div>
     </div>
   );
